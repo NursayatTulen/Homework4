5a217e3d47a9aa217ff9591db29c2e29642914be
NursayatTulen <nuralemtulen@gmail.com>
2025-04-05 18:12:51 +0600

    Added MarkerStyle.java /flyweith

---
+++ src/flyweight/MarkerStyle.java
@@ -0,0 +1,11 @@
+package flyweight;
+
+public class MarkerStyle {
+    String icon;
+    String color;
+
+    public MarkerStyle(String icon, String color) {
+        this.icon = icon;
+        this.color = color;
+    }
+}
